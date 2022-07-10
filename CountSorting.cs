35b848be258e79b8c710421c6220cc664dbbbd4a
Koval Vadym <vzlobinkov@gmail.com>
2022-07-10 22:13:57 +0300

    6l

--- CountSorting.cs
+++ CountSorting.cs
@@ -10,26 +10,34 @@ namespace AlgoTheory_LAB6
         public static void Sort(ref char[] array)
         {
             int n = array.Length;
-
+            // The output character array that
+            // will have sorted arr
             char[] output = new char[n];
-
+            /// Create a count array to store
+            // count of individual characters
+            // and initialize count array as 0
             int[] count = new int[256];
 
             for (int i = 0; i < 256; ++i)
                 count[i] = 0;
-
+            // store count of each characte
             for (int i = 0; i < n; ++i)
                 ++count[array[i]];
-
+            // Change count[i] so that count[i]
+            // now contains actual position of
+            // this character in output array
             for (int i = 1; i <= 255; ++i)
                 count[i] += count[i - 1];
-
+            // Build the output character array
+            // To make it stable we are operating in reverse order.
             for (int i = n - 1; i >= 0; i--)
             {
                 output[count[array[i]] - 1] = array[i];
                 --count[array[i]];
             }
-
+            // Copy the output array to arr, so
+            // that arr now contains sorted
+            // characters
             for (int i = 0; i < n; ++i)
                 array[i] = output[i];
         }
--- HeapSorting.cs
+++ HeapSorting.cs
@@ -10,38 +10,43 @@ namespace AlgoTheory_LAB6
         {
             int n = array.Length;
 
+            // Побудова купи
             for (int i = n / 2 - 1; i >= 0; i--)
                 Heapify(array, n, i);
-
+            // Один за іншим "витягуємо" элементи з купи
             for (int i = n - 1; i >= 0; i--)
             {
+                // Рухаємо поточний корінь в кінець
                 int temp = array[0];
                 array[0] = array[i];
                 array[i] = temp;
-
+                // Викликаємо процедуру heapify на зменшенній купі
                 Heapify(array, i, 0);
             }
         }
 
+        // Метод для зміни в двійкову купу піддерева з кореневим вузлом i, що є
+        // індексом в arr[]. n - розмір купи
         public static void Heapify(int[] array, int n, int i)
         {
             int largest = i;
+            // Ініціалізуємо найбільший елемент як корінь
 
             int l = 2 * i + 1; // left = 2*i + 1
             int r = 2 * i + 2; // right = 2*i + 2
-
+            // Якщо лівий дочірній елемент більше кореня
             if (l < n && array[l] > array[largest])
                 largest = l;
-
+            // Якщо правий дочірній елемент більше, ніж найбільший елемент на даний момент
             if (r < n && array[r] > array[largest])
                 largest = r;
-
+            // Якщо найбільший елемент - не корінь
             if (largest != i)
             {
                 int swap = array[i];
                 array[i] = array[largest];
                 array[largest] = swap;
-
+                // Рекурсивно змінюємо в двійкову купу дане піддерево
                 Heapify(array, n, largest);
             }
         }
